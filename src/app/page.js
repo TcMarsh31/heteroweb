b48e0900cfe34a59b87405d350eddd2e4f0b08c9
knirojini <niroginikunalan@gmail.com>
2025-12-24 20:16:17 +0530

    code refacor

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,5 @@
-"use client";
-
-import Navbar from "@/components/navbar";
-import Hero from "@/components/Hero";
+import Home from "./(home)";
 
 export default function App() {
-  return (
-    <>
-      {/* Hero Section */}
-      <Hero />
-
-      {/* Page Content */}
-      <section className="bg-red-500 p-10 text-white">
-        <h2 className="mb-4 text-2xl font-bold">What is Lorem Ipsum?</h2>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
-        </p>
-      </section>
-    </>
-  );
+  return <Home />;
 }

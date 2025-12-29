78180f21effdd51452e1c00db61fd321ff561b2f
knirojini <niroginikunalan@gmail.com>
2025-12-29 19:45:39 +0530

    add blog ui

---
+++ src/app/blog/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+import BlogLayout from ".";
+
+function Blog() {
+  return <BlogLayout />;
+}
+
+export default Blog;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -139,6 +139,9 @@ export default function Navbar() {
           <Link href="#projects" className="font-medium">
             Projects
           </Link>
+          <Link href="#projects" className="font-medium">
+            Blog
+          </Link>
         </nav>
 
         {/* Right */}

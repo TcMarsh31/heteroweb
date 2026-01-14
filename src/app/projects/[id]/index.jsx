b7886d30ee6853109db1e375757d390d106ace02
knirojini <niroginikunalan@gmail.com>
2026-01-14 20:43:43 +0530

    code refactor

--- src/app/projects/[id]/index.jsx
+++ src/app/projects/[id]/index.jsx
@@ -1,7 +1,5 @@
 "use client";
 import SecoundHero from "@/components/SecoundHero";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { ListChecks, Brain, MessageSquare } from "lucide-react";
 import QT from "./qt";
 import GrocXpress from "./grocx";
 
